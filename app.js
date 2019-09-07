face30a697b4793afc406edad7b83003d4aa932e
Arkansio <38956983+Arkansio@users.noreply.github.com>
2019-09-07 03:49:22 +0200

    increase performances

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 var torrentstream = require('torrent-stream');
@@ -8,36 +10,17 @@ app.listen(5050, () => {
   console.log(`App is running on http://localhost:5050`);
 })
 
-const magnet = 'magnet:?xt=urn:btih:BDBEB3CCFC6D3C844C8468B998F02BC09405A7D0&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337';
+const magnet = 'magnet:?xt=urn:btih:39DE6A085A5ADE86E775FFF702BB038396C391DD&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337';
 let path;
 
 const options = {
-  tmp: __dirname + '/tmp'
+  tmp: __dirname + '/tmp',
+  path: __dirname + '/tmp/files'
 }
 
-let engine = torrentstream(magnet, options);
-
-engine.on('ready', function () {
-  engine.files.forEach(function (file) {
-    console.log('filename:', file.name);
-    console.log(`length ${file.length}`);
-    console.log(`path: ${file.path}`)
-    if (file.filename.includes('.mp4'))
-      path = file.path;
-    var stream = file.createReadStream();
-  })
-})
-
-engine.on('download', function (name, unknown) {
-  console.log('downloaded')
-  console.log(name);
-  console.log(unknown)
-})
-
-app.get('/video', (req, res) => {
+const streamTorrent = function(req, res, fl) {
   console.log('path', path)
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
+  const fileSize = fl.length
   const range = req.headers.range
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-")
@@ -46,7 +29,7 @@ app.get('/video', (req, res) => {
       ? parseInt(parts[1], 10)
       : fileSize - 1
     const chunksize = (end - start) + 1
-    const file = fs.createReadStream(path, { start, end })
+    const file = fl.createReadStream({ start, end })
     const head = {
       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
       'Accept-Ranges': 'bytes',
@@ -54,6 +37,7 @@ app.get('/video', (req, res) => {
       'Content-Type': 'video/mp4',
     }
     res.writeHead(206, head);
+    console.log('streaming directly from torrent')
     file.pipe(res);
   } else {
     const head = {
@@ -63,4 +47,38 @@ app.get('/video', (req, res) => {
     res.writeHead(200, head)
     fs.createReadStream(path).pipe(res)
   }
+}
+
+let engine;
+
+app.get('/video', (req, res) => {
+  console.log('request to video !')
+  if (engine) {
+    console.log('destroy engine');
+    engine.destroy();
+  }
+  engine = torrentstream(magnet, options);
+  let fileSize;
+
+  engine.on('ready', function () {
+    console.log('ready');
+    engine.files.forEach(function (file) {
+      console.log('filename:', file.name);
+      console.log(`length ${file.length}`);
+      console.log(`path: ${file.path}`)
+      if (file.name.includes('.mp4')) {
+        path = `${__dirname}/tmp/files/${file.path}`;
+        fileSize = file.length;
+        console.log('stream torrent')
+        streamTorrent(req, res, file)
+      }
+    })
+  })
+
+  engine.on('download', function (piece) {
+    const percent = Math.round(engine.swarm.downloaded / fileSize * 100 * 100) / 100;
+    console.log('downloaded')
+    console.log(piece);
+    console.log(percent)
+  })
 })
