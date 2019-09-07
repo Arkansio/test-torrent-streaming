9e8e9895a2f7e93fa0fbf5f1b7e3e6c04dfdb995
Arkansio <38956983+Arkansio@users.noreply.github.com>
2019-09-07 03:53:19 +0200

    remove magnet string

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(5050, () => {
   console.log(`App is running on http://localhost:5050`);
 })
 
-const magnet = 'magnet:?xt=urn:btih:39DE6A085A5ADE86E775FFF702BB038396C391DD&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337';
+const magnet = process.env.magnet;
 let path;
 
 const options = {
